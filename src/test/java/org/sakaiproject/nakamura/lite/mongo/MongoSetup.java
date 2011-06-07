cf794e3f1d9e9f7c42ccd2157145749124bb1d55
Erik Froese <erik.froese@gmail.com>
2011-06-07 18:01:35 -0400

    Add the testing skeleton.

---
+++ src/test/java/org/sakaiproject/nakamura/lite/mongo/MongoSetup.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.lite.mongo;
+
+import java.net.UnknownHostException;
+
+import org.sakaiproject.nakamura.lite.storage.mongo.MongoClientPool;
+
+import com.google.common.collect.ImmutableMap;
+import com.mongodb.MongoException;
+
+public class MongoSetup {
+
+    private static MongoClientPool clientPool = createClientPool();
+
+    public synchronized static MongoClientPool createClientPool() {
+        try {
+        	MongoClientPool connectionPool = new MongoClientPool();
+            connectionPool
+                    .activate(ImmutableMap
+                            .of(MongoClientPool.PROP_MONGO_URI, (Object) "mongodb://127.0.0.1",
+                            	MongoClientPool.PROP_MONGO_DB, (Object) "nakamura"));
+            return connectionPool;
+        } catch (MongoException e) {
+        	throw new RuntimeException(e.getMessage(), e);
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+    }
+
+    public static MongoClientPool getClientPool() {
+        return clientPool;
+    }
+}
