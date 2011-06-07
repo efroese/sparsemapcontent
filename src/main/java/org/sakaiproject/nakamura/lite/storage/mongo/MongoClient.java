0d2e4f923045bd701526bda513b6da8276a06081
Erik Froese <erik.froese@gmail.com>
2011-06-07 18:00:54 -0400

    Use a FileStreamContentHelper to write out files to the FS. Copy the rowhasher fucntion from the JDBCStorageClient

--- src/main/java/org/sakaiproject/nakamura/lite/storage/mongo/MongoClient.java
+++ src/main/java/org/sakaiproject/nakamura/lite/storage/mongo/MongoClient.java
@@ -2,36 +2,61 @@ package org.sakaiproject.nakamura.lite.storage.mongo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 import java.util.Map;
 
 import org.sakaiproject.nakamura.api.lite.StorageClientException;
+import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
 import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
+import org.sakaiproject.nakamura.lite.content.FileStreamContentHelper;
+import org.sakaiproject.nakamura.lite.content.InternalContent;
+import org.sakaiproject.nakamura.lite.content.StreamedContentHelper;
 import org.sakaiproject.nakamura.lite.storage.DisposableIterator;
+import org.sakaiproject.nakamura.lite.storage.RowHasher;
 import org.sakaiproject.nakamura.lite.storage.StorageClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.ImmutableMap;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 
-public class MongoClient implements StorageClient {
+public class MongoClient implements StorageClient, RowHasher {
+
+	private static final Logger log = LoggerFactory.getLogger(MongoClient.class);
 
 	private DB mongodb;
-	
-	public MongoClient(DB mongodb) {
+
+	private StreamedContentHelper streamedContentHelper;
+
+	public MongoClient(DB mongodb, Map<String,Object> props) {
+		log.debug("Created");
 		this.mongodb = mongodb;
 		this.mongodb.authenticate("admin", "admin".toCharArray());
+		this.mongodb.requestStart();
+
+		this.streamedContentHelper = new FileStreamContentHelper(null, props);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.sakaiproject.nakamura.lite.storage.StorageClient#get(java.lang.String, java.lang.String, java.lang.String)
+	 */
 	@SuppressWarnings("unchecked")
 	public Map<String, Object> get(String keySpace, String columnFamily,
 			String key) throws StorageClientException {
 		DBCollection collection = mongodb.getCollection(columnFamily);
+
+		// Pretty straightforward. Just query by the _id.
 		BasicDBObject query = new BasicDBObject();
 		query.put("_id", key);
 		DBCursor cursor = collection.find(query);
-		Map<String,Object> result = null; 
+		Map<String,Object> result = null;
 		if (cursor.size() == 1){
 			result = (Map<String,Object>)cursor.next();
 		}
@@ -40,7 +65,7 @@ public class MongoClient implements StorageClient {
 
 	public void insert(String keySpace, String columnFamily, String key,
 			Map<String, Object> values, boolean probablyNew)
-			throws StorageClientException {
+	throws StorageClientException {
 		DBCollection collection = mongodb.getCollection(columnFamily);
 		BasicDBObject toInsert = new BasicDBObject();
 		MongoClientUtils.copyToDBObject(toInsert, values);
@@ -48,7 +73,7 @@ public class MongoClient implements StorageClient {
 	}
 
 	public void remove(String keySpace, String columnFamily, String key)
-			throws StorageClientException {
+	throws StorageClientException {
 		DBCollection collection = mongodb.getCollection(columnFamily);
 		BasicDBObject query = new BasicDBObject();
 		query.put("_id", key);
@@ -59,16 +84,15 @@ public class MongoClient implements StorageClient {
 			String contentId, String contentBlockId, String streamId,
 			Map<String, Object> content) throws StorageClientException,
 			AccessDeniedException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return streamedContentHelper.readBody(keySpace, columnFamily, contentBlockId, streamId, content);
 	}
 
 	public Map<String, Object> streamBodyIn(String keySpace,
 			String columnFamily, String contentId, String contentBlockId,
 			String streamId, Map<String, Object> content, InputStream in)
 			throws StorageClientException, AccessDeniedException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String,Object> meta = streamedContentHelper.writeBody(keySpace, columnFamily, contentId, contentBlockId, streamId, content, in);
+		return meta;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -77,7 +101,7 @@ public class MongoClient implements StorageClient {
 			throws StorageClientException {
 		DBCollection collection = mongodb.getCollection(columnFamily);
 		BasicDBObject query = new BasicDBObject();
-	
+
 		MongoClientUtils.copyToDBObject(query, properties);
 		DBCursor cursor = collection.find(query);
 		Iterator<?> itr = (Iterator<?>)cursor.iterator();
@@ -85,18 +109,35 @@ public class MongoClient implements StorageClient {
 	}
 
 	public void close() {
-		// TODO Auto-generated method stub
+		log.debug("Closed");
+		this.mongodb.requestDone();
 	}
 
 	public DisposableIterator<Map<String, Object>> listChildren(
 			String keySpace, String columnFamily, String key)
 			throws StorageClientException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public boolean hasBody(Map<String, Object> content, String streamId) {
-		// TODO Auto-generated method stub
-		return false;
+		return streamedContentHelper.hasStream(content, streamId);
+	}
+
+	public String rowHash(String keySpace, String columnFamily, String key)
+	throws StorageClientException {
+		MessageDigest hasher;
+		try {
+			hasher = MessageDigest.getInstance("SHA1");
+		} catch (NoSuchAlgorithmException e1) {
+			throw new StorageClientException("Unable to get hash algorithm " + e1.getMessage(), e1);
+		}
+		String keystring = keySpace + ":" + columnFamily + ":" + key;
+		byte[] ridkey;
+		try {
+			ridkey = keystring.getBytes("UTF8");
+		} catch (UnsupportedEncodingException e) {
+			ridkey = keystring.getBytes();
+		}
+		return StorageClientUtils.encode(hasher.digest(ridkey));
 	}
 }
